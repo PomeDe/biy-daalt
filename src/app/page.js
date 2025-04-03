22b5e4d0dac4fc91a302ff13e8a4def163b7913e
Rana <93430019+PomeDe@users.noreply.github.com>
2025-04-03 14:40:05 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -29,14 +29,20 @@ export default function Home() {
     const router = useRouter()
     return(
 
-        <div className="flex items-center  h-screen w-screen bg-slate-500 flex-col space-x-5 space-y-64 bg-no-repeat bg-center bg-cover bg-[url(https://wallpapers.com/images/hd/hacker-background-uxvhaqwmbsp6qoth.jpg)]">
-            <h1 className="text-white font-bold text-5xl font-sans mt-5 bg-black">11V B.Anar</h1>
+        <div className="flex items-center  h-screen w-screen  flex-col space-x-5 space-y-64 bg-no-repeat bg-gradient-to-br from-slate-300 via-gray-500 to-slate-800">
+            <div className='flex flex-row w-full h-1/6 items-center justify-evenly' >
+            <img src='https://th.bing.com/th/id/R.60549d44ebf9e6fcac9b1b1ec86bc8ad?rik=nFDJeCSIoHfsWA&pid=ImgRaw&r=0&sres=1&sresct=1' className='w-32 h-32 mt-6 rounded-4xl'/>
+            <h1 className="text-white font-bold text-5xl font-sans mt-5 ">11V B.Anar</h1>
+            <div className='w-16'></div>
+            
+            </div>
+            
             <div className="flex flex-row  grid-cols-4 gap-24 " 
             >
             {data.map((element)=>(
                           <div
                           key={element.id}
-                          className="w-84 h-72 flex flex-col p-5 rounded-lg text-black shadow-xl border-black bg-slate-200 transition delay-100 duration-200 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-blue-400"
+                          className="w-84 h-72 flex flex-col p-5 rounded-lg text-black shadow-xl border-black bg-slate-200 transition delay-100 duration-200 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-neutral-500"
                           onClick={()=>router.push(element.file)}
                         >
                           <div
@@ -47,8 +53,9 @@ export default function Home() {
                           />
                           </div>
               
-                          <div className="text-black ml-2">
-                            <p className="text-black">Firstname: {element.name}</p>
+                          <div className="text-black ml-2 flex flex-row space-x-3">
+                            <p className="text-black">Project name: </p>
+                            <p className='text-blue-500 font-bold'>{element.name}</p>
                           </div>
                         </div>
             ))}
